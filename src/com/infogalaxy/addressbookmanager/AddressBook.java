8abbadd4f066c4354caa87152d36fe19471d9bee
ujwaladhangar <ujwaladhangar037@gmail.com>
2022-03-31 20:19:23 +0530

    MODIFIED: UC-6-Backup TO File

--- src/com/infogalaxy/addressbookmanager/AddressBook.java
+++ src/com/infogalaxy/addressbookmanager/AddressBook.java
@@ -1,5 +1,9 @@
 package com.infogalaxy.addressbookmanager;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -42,14 +46,14 @@ public class AddressBook {
         int flag = 0;
         for (int i = 0; i < contactList.size(); i++) {
             Contact contact = contactList.get(i);
-            if(fname.equals((contact.getFirstName()))){
+            if (fname.equals((contact.getFirstName()))) {
                 flag = 1;
                 break;
             }
         }
-        if ( flag == 0){
-        System.out.println("Contact Not Found. ");
-    }else {
+        if (flag == 0) {
+            System.out.println("Contact Not Found. ");
+        } else {
             System.out.println("Contact is Found .");
             Contact contact = new Contact();
             System.out.println("Enter The Last Name : ");
@@ -68,24 +72,48 @@ public class AddressBook {
             contact.setZip(sc.next());
         }
     }
-    public void searchState(){
+
+    public void searchState() {
         System.out.println("Enter the State Name You Want to Search . ");
         String stateName = sc.next();
+        int total = 0;
         for (int i = 0; i < contactList.size(); i++) {
             Contact contact = contactList.get(i);
             if (stateName.equals(contact.getState())) {
                 System.out.println(contact.getFirstName());
                 System.out.println(contact.getMobno());
+                total = total + 1;
             }
         }
+        System.out.println("Total Contact Having Same State is : " + total);
     }
-    public void deleteContact(){
-        System.out.println("Enter The First Name : ");
+
+    public void deleteContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Contact Name : ");
         String delete = sc.next();
 
         for (int i = 0; i < contactList.size(); i++) {
             Contact contact = contactList.get(i);
-            if (delete.equals(contactList.remove(i))) {
+            if (delete.equals(contact.getFirstName())) {
+                contactList.remove(i);
+            }
+        }
+    }
+
+    public void backupToFile() {
+        String contactData = null;
+        for (int i = 0; i < contactList.size(); i++) {
+            Contact contact = contactList.get(i);
+            contactData = contact.getFirstName() + "," + contact.getLastName() + "," + contact.getAddress() + "," + contact.getCity()
+                    + "," + contact.getState() + "," + contact.getMobno() + "," + contact.getEmail() + "," + contact.getZip() + "\n" + contactData;
+
+            try {
+                Path file = Paths.get("MyData.txt");
+                byte[] fileData = contactData.getBytes();
+                Files.write(file, fileData);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -94,29 +122,32 @@ public class AddressBook {
         Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
         int choice;
-      do {
-          System.out.println(" **** CONTACT LIST MANAGEMENT **** ");
-          System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EDIT CONTACT \n4. SEARCH STATE \n5. DELETE CONTACT \n6. EXIT ");
-          System.out.println("Enter Your Choice : ");
-          choice = sc.nextInt();
-          switch (choice) {
-              case 1:
-                  addressBook.addcontact();
-                  break;
-              case 2:
-                  addressBook.displayContact();
-                  break;
-              case 3:
-                  addressBook.editContact();
-                  break;
-              case 4:
-                  addressBook.searchState();
-                  break;
-              case 5:
-                  addressBook.deleteContact();
-                  break;
-          }
-      }while (choice !=6);
+        do {
+            System.out.println(" **** CONTACT LIST MANAGEMENT **** ");
+            System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EDIT CONTACT \n4. SEARCH STATE \n5. DELETE CONTACT \n6. BACKUP TO FILE \n7. EXIT ");
+            System.out.println("Enter Your Choice : ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addressBook.addcontact();
+                    break;
+                case 2:
+                    addressBook.displayContact();
+                    break;
+                case 3:
+                    addressBook.editContact();
+                    break;
+                case 4:
+                    addressBook.searchState();
+                    break;
+                case 5:
+                    addressBook.deleteContact();
+                    break;
+                case 6:
+                    addressBook.backupToFile();
+                    break;
+            }
 
+        } while (choice != 7);
     }
 }
